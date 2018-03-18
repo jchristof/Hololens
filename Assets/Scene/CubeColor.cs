a248bfee7aeb7575d5357db266d7e6b8e17e2ad9
Jon Martin <jchristofmartin@gmail.com>
2018-03-17 20:45:09 -0500

    adding cursor color selection

--- Assets/Scene/CubeColor.cs
+++ Assets/Scene/CubeColor.cs
@@ -1,17 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class CubeColor : MonoBehaviour {
 
+    private Color originalColor;
+
 	// Use this for initialization
 	void Start () {
 		var newColor = new Color(Random.Range((float)0.0, (float)1.0), Random.Range((float)0.0, (float)1.0), Random.Range((float)0.0, (float)1.0));
-	    this.GetComponent<Renderer>().material.color = newColor;
+	    GetComponent<Renderer>().material.color = newColor;
+	    originalColor = newColor;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void OnSelect() {
+        var cube = GetComponent<Renderer>();
+        cube.material.color = Color.magenta;
+    }
+    void OnReset() {
+        var cube = GetComponent<Renderer>();
+        cube.material.color = originalColor;
+    }
 }
--- Assets/Scene/xCursor.cs
+++ Assets/Scene/xCursor.cs
@@ -6,6 +6,7 @@ namespace Assets.Scene {
     public class xCursor : MonoBehaviour {
 
         private MeshRenderer meshRenderer;
+        public GameObject FocusedObject { get; private set; }
 
         // Use this for initialization
         void Start () {
@@ -14,6 +15,7 @@ namespace Assets.Scene {
 	
         // Update is called once per frame
         void Update () {
+            meshRenderer.enabled = true;
             var headPosition = Camera.main.transform.position;
             var gazeDirection = Camera.main.transform.forward;
 
@@ -24,9 +26,25 @@ namespace Assets.Scene {
 
                 transform.position = hitInfo.point;
                 transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+
+                var newFocusedObject = hitInfo.collider.gameObject;
+
+                if (FocusedObject != null && newFocusedObject != FocusedObject) {
+                    FocusedObject.SendMessage("OnReset");
+                }
+
+                FocusedObject = newFocusedObject;
+                FocusedObject.SendMessage("OnSelect");
+
             }
-            else
+            else {
                 meshRenderer.enabled = false;
+
+                if(FocusedObject != null)
+                    FocusedObject.SendMessage("OnReset");
+
+                FocusedObject = null;
+            }
         }
 
     }
