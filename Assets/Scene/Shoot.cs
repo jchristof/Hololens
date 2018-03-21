742c88dd9d33830a4fbc2a56f2b868b8dd126d1f
Jon Martin <jchristofmartin@gmail.com>
2018-03-20 22:48:23 -0500

    adding shot class

---
+++ Assets/Scene/Shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class Shoot : MonoBehaviour {
+
+    private GestureRecognizer recognizer;
+    public float ForceMagnitude = 300f;
+
+	// Use this for initialization
+	void Start () {
+        recognizer = new GestureRecognizer();
+	    recognizer.TappedEvent += ShootBall;
+        recognizer.StartCapturingGestures();
+	}
+
+    private void ShootBall(InteractionSourceKind source, int tapCount, Ray headRay) {
+        var ball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        ball.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        var rigidBody = ball.AddComponent<Rigidbody>();
+        rigidBody.mass = 0.5f;
+        rigidBody.position = transform.position;
+        var transformForward = transform.forward;
+        transformForward = Quaternion.AngleAxis(-10, transform.right) * transformForward;
+        rigidBody.AddForce(transformForward * ForceMagnitude);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
