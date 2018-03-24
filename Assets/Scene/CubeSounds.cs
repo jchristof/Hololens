96f49d93e3c3bbafa3200ed5f766b2d67af9ddfe
Jon Martin <jchristofmartin@gmail.com>
2018-03-23 22:20:08 -0500

    adding audio

---
+++ Assets/Scene/CubeSounds.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSounds : MonoBehaviour {
+
+    AudioSource audioSource;
+
+    private AudioClip impactClip;
+
+	// Use this for initialization
+	void Start () {
+	    audioSource = gameObject.AddComponent<AudioSource>();
+	    audioSource.playOnAwake = false;
+	    audioSource.spatialize = true;
+	    audioSource.spatialBlend = 1.0f;
+	    audioSource.dopplerLevel = 0.0f;
+	    audioSource.rolloffMode = AudioRolloffMode.Logarithmic;
+	    audioSource.maxDistance = 20f;
+
+	    impactClip = Resources.Load<AudioClip>("punch");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision) {
+        audioSource.clip = impactClip;
+        audioSource.Play();
+    }
+}
--- Assets/Scene/Shoot.cs
+++ Assets/Scene/Shoot.cs
@@ -8,12 +8,27 @@ public class Shoot : MonoBehaviour {
     private GestureRecognizer recognizer;
     public float ForceMagnitude = 300f;
 
+    private AudioSource audioSource;
+
+    private AudioClip shootClip;
+
 	// Use this for initialization
 	void Start () {
         recognizer = new GestureRecognizer();
 	    recognizer.TappedEvent += ShootBall;
         recognizer.StartCapturingGestures();
-	}
+
+	    audioSource = gameObject.AddComponent<AudioSource>();
+	    audioSource.playOnAwake = false;
+	    audioSource.spatialize = true;
+	    audioSource.spatialBlend = 1.0f;
+	    audioSource.dopplerLevel = 0.0f;
+	    audioSource.rolloffMode = AudioRolloffMode.Logarithmic;
+	    audioSource.maxDistance = 20f;
+	    audioSource.volume = .25f;
+
+	    shootClip = Resources.Load<AudioClip>("shoot");
+    }
 
     private void ShootBall(InteractionSourceKind source, int tapCount, Ray headRay) {
         var ball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
@@ -24,6 +39,9 @@ public class Shoot : MonoBehaviour {
         var transformForward = transform.forward;
         transformForward = Quaternion.AngleAxis(-10, transform.right) * transformForward;
         rigidBody.AddForce(transformForward * ForceMagnitude);
+
+        audioSource.clip = shootClip;
+        audioSource.Play();
     }
 	// Update is called once per frame
 	void Update () {}
